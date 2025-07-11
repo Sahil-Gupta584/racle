e0174e6abc2f8b06d627634f79c6520c101f4381
Sahil Gupta <guptas3067@gmail.com>
2025-07-11 14:43:13 +0530

    g

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config({ path: resolve(__dirname, "../../../../.env") });
 console.log({
+  source: "auth.ts",
   "process.env.GOOGLE_CLIENT_ID": process.env.GOOGLE_CLIENT_ID,
   "process.env.DATABASE_URL": process.env.DATABASE_URL,
 });
