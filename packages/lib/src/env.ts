578ae5b104a44517c7ae93dd0a3ae0cecb2d2e10
Sahil Gupta <guptas3067@gmail.com>
2025-07-29 17:03:51 +0530

    3

--- packages/lib/src/env.ts
+++ packages/lib/src/env.ts
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import z from "zod";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../../../secret/.env") });
+dotenv.config({
+  path: [path.resolve(__dirname, "../../../secret/.env"), "/etc/secrets/.env"],
+});
 
 const dummyEnv = {
   GOOGLE_CLIENT_ID: "1234567890abcdef.apps.googleusercontent.com",
@@ -67,6 +70,9 @@ export const parseRes = envSchema.safeParse(process.env);
 // export const parseRes = envSchema.safeParse(dummyEnv);
 
 if (!parseRes.success) {
+  const content = fs.readFileSync("/etc/secrets/.env", "utf8");
+  console.log({ content });
+
   console.error("Environment variables validation failed:", parseRes.error);
   throw new Error("Invalid environment variables");
 }
